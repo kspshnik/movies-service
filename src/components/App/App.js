50fe13af6593ca5303ed0cbf2810555947bbbcd4
Evgeny Karpel <kspshnik@yandex.ru>
2021-05-13 20:44:37 +0300

    new: заголовок и App - роуты и получение количества колонок

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,45 @@
-import logo from '../../logo.svg';
+import React, { useState, useEffect } from "react";
+import { Switch, Route, useHistory } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const getMoviesCount = () => {
+    const rootElement = document.documentElement;
+    return parseInt(getComputedStyle(rootElement).getPropertyValue('--movies_count'));
+  }
+  const [columnsCount, setColumnsCount] = useState(getMoviesCount());
+
+  useEffect(() => {
+    const setColumns = () => {
+      setColumnsCount(getMoviesCount());
+    };
+    window.addEventListener('resize', setColumns);
+    return () => window.removeEventListener('resize', setColumns);
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page typo">
+     <Header isLoggedIn = {isLoggedIn} />
+      <Switch>
+        <Route path='/signin'>
+         <SignInPage />
+        </Route>
+        <Route path='/signup'>
+      <SignUpPage />
+      </Route>
+      <Route path='/profile'>
+        <ProfilePage />
+      </Route>
+      <Route path='/movies'>
+       <MoviesPage />
+      </Route>
+      <Route path='saved-movies'>
+        <FavoritesPage />
+      </Route>
+      <Route exact path='/'>
+      <LandingPage />
+      </Route>
+    </Switch>
+  //    <Footer />
     </div>
   );
 }
