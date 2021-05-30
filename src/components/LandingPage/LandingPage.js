2f6765e9fc8666368232810571cf0ab314466808
Evgeny Karpel <kspshnik@yandex.ru>
2021-05-31 02:13:17 +0300

    feature: лендинг секция about

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -21,19 +21,21 @@ const LandingPage = () => (
     <section id='about' className='landing__section'>
       <h2 className='landing__title'>О проекте</h2>
       <div className='landing__separator' />
-      <h3 className='landing__subtitle'>Дипломный проект включал 5 этапов</h3>
-      <p className='landing__text'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
-      <h3 className='landing__subtitle'>На выполнение диплома ушло 5 недель</h3>
-      <p className='landing__text'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+      <div className='landing__about'>
+        <div className='landing__item'>
+          <h3 className='landing__subtitle'>Дипломный проект включал 5 этапов</h3>
+          <p className='landing__text'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+        </div>
+        <div className='landing__item'>
+          <h3 className='landing__subtitle'>На выполнение диплома ушло 5 недель</h3>
+          <p className='landing__text'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        </div>
+      </div>
       <div className='landing__timing'>
-        <div className='landing__timeline'>
-          <div className='landing__backend landing__backend_timeline'>1 неделя</div>
-          <div className='landing__frontend landing__frontend_timeline'>4 недели</div>
-        </div>
-        <div className='landing__timeline'>
-          <div className='landing__backend_cutline'>Back-end</div>
-          <div className='landing__frontend_cutline'>Front-end</div>
-        </div>
+        <div className='landing__backend-timeline'>1 неделя</div>
+        <div className='landing__frontend-timeline'>4 недели</div>
+        <div className='landing__cutline'>Back-end</div>
+        <div className='landing__cutline'>Front-end</div>
       </div>
     </section>
     <section id='tech' className='landing__section'>
